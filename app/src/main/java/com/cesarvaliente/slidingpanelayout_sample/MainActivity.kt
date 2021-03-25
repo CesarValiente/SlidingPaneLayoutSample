07d18725aad1a291880494d7616b50bedee5ed30
Craig Dunn <craig.dunn@microsoft.com>
2021-03-25 16:36:32 -0700

    exploring the slide pane

--- app/src/main/java/com/cesarvaliente/slidingpanelayout_sample/MainActivity.kt
+++ app/src/main/java/com/cesarvaliente/slidingpanelayout_sample/MainActivity.kt
@@ -1,21 +1,41 @@
 package com.cesarvaliente.slidingpanelayout_sample
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.slidingpanelayout.widget.SlidingPaneLayout
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "SLIDEPANE"
+    lateinit var sidePane: SlidingPaneLayout
+    lateinit var open: Button
+    //lateinit var close: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val sidePane = findViewById<SlidingPaneLayout>(R.id.sliding_pane_layout)
-        val open = findViewById<Button>(R.id.side_open)
-        val close = findViewById<Button>(R.id.side_close)
+        sidePane = findViewById<SlidingPaneLayout>(R.id.sliding_pane_layout)
+        open = findViewById<Button>(R.id.side_open)
+        //close = findViewById<Button>(R.id.side_close)
 
         open.setOnClickListener { sidePane.open() }
-        close.setOnClickListener { sidePane.close() }
+        //close.setOnClickListener { sidePane.close() }
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+
+        if (sidePane.isSlideable)
+        {
+            Log.i(TAG, "sidePane.isSlideable")
+        }
+        else
+        {
+            Log.i(TAG, "NOT sidePane.isSlideable")
+        }
     }
 }
